24809822047c6473bd8757b0b6aa951a9da76f38
Moggoww <gauthier.mauche@gmail.com>
2022-04-13 22:18:57 +0200

    Add homme page and character components

---
+++ components/Character.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+      HOME
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+
+</script>
---
+++ components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      HOME
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      character: [],
+    };
+  },
+  created: async function () {
+    let datas = await axios.get("https://rickandmortyapi.com/api/character");
+    console.log(datas.data.results);
+    this.character = datas.data;
+  },
+};
+</script>
