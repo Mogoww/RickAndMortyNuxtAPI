1e297a404a855e8ea460e94db555c82f3f0467a2
Moggoww <gauthier.mauche@gmail.com>
2022-04-18 21:25:43 +0200

    custom display character

--- components/Character.vue
+++ components/Character.vue
@@ -8,36 +8,128 @@
     <div v-else>
       <div>
         <div class="character-img">
-          <img
-            :src="character.image"
-            :alt="character.name"
-            style="width: 50%"
-          />
+          <img :src="character.image" :alt="character.name" />
         </div>
       </div>
-      name: {{ character.name }} <br />
-      <!-- {{ character }} -->
-      status: {{ character.status }} <br />
-      species: {{ character.species }} <br />
-      type: {{ character.type }} <br />
-      gender: {{ character.gender }} <br />
-      status: {{ character.status }} <br />
+
+      <div>
+        <div class="bg-green-200 py-32 px-10">
+          <div class="bg-white p-10 md:w-3/4 lg:w-1/2 mx-auto">
+            <div>
+              <div class="flex items-center mb-5">
+                <div class=" inline-block w-20 mr-6 text-right font-bold text-gray-600">
+                  Nom
+                </div>
+
+                <div class="flex-1 py-2 border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" >
+                  {{ character.name }}
+                </div>
+              </div>
+
+              <div class="flex items-center mb-10">
+                <div class="flex relative">
+                  <div class="relative inline-block w-20 mr-6 text-right font-bold text-gray-600">
+                    Status                  
+                  </div>
+                  <div v-if="character.status == 'Alive'">
+                    <div class="absolute right-4 shadow-sm bg-green-500 w-2 h-2 rounded-full"></div>
+                  </div>
+                  <div v-else-if="character.status == 'Dead'">
+                    <div class="absolute right-4 shadow-sm bg-red-600 w-2 h-2 rounded-full"></div>
+                  </div>
+                  <div v-else>
+                    <div class="absolute right-4 shadow-sm bg-gray-600 w-2 h-2 rounded-full"></div>
+                  </div>
+
+                </div>
+                <div class="flex-1 flex py-2 border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" >
+                  <span v-if="character.status == 'unknown' ">
+                    Manquant                  
+                  </span>
+                  <span v-else>
+                    {{ character.status }} 
+                  </span>
+                </div>
+              </div>
+
+              <div class="flex items-center mb-10">
+                <div class=" inline-block w-20 mr-6 text-right font-bold text-gray-600">
+                  species
+                </div>
+
+                <div class="flex-1 py-2 border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" >
+                  {{ character.species }}
+                </div>
+              </div>
+
+              <div class="flex items-center mb-10">
+                <div class=" inline-block w-20 mr-6 text-right font-bold text-gray-600">
+                  gender
+                </div>
+
+                <div class="flex-1 py-2 border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" >
+                  {{ character.gender }}
+                </div>
+              </div>
+
+              <div class="flex items-center mb-10">
+                <div class=" inline-block w-20 mr-6 text-right font-bold text-gray-600">
+                  type
+                </div>
+
+                <div class="flex-1 py-2 border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" >
+                  <!-- {{ character }} -->
+                </div>
+              </div>
+
+             
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- <div>name: {{ character.name }}</div>
+      <div>status: {{ character.status }}</div>
+      <div>species: {{ character.species }}</div>
+      <div>gender: {{ character.gender }}</div>
+      <div>type: {{ character.type }}</div>
+      <div>status: {{ character.status }}</div>
 
       <div>
         <h1>origin</h1>
-        <!-- name: {{character.origin.name}} -->
-      </div>
+        <div v-if="character.origin">
+          {{ character.origin }}
+          {{ character.origin["name"] }}
+        </div>
+        <div v-if="character.origin">
+          {{ character.origin["url"] }}
+        </div>
+     {{character.origin["url"]}} -->
+      <!-- </div>
 
-      <!-- "origin": { "name": "unknown", "url": "" }, "location": { "name": "Earth (Replacement Dimension)", "url": "https://rickandmortyapi.com/api/location/20" }, "image": "https://rickandmortyapi.com/api/character/avatar/13.jpeg", "episode": [ "https://rickandmortyapi.com/api/episode/31" ], "url": "https://rickandmortyapi.com/api/character/13", "created": "2017-11-04T20:33:30.779Z" } -->
+      <div>
+        <h1>location</h1>
+        <div v-if="character.location">
+          {{ character.location }}
+          {{ character.location["name"] }}
+        </div>
+        <div v-if="character.origin">
+          {{ character.location["url"] }}
+        </div>
+        {{character.origin["url"]}} -->
+      <!--  </div> -->
+      <!-- "origin": { "name": "unknown", "url": "" }, "location": { "name": "Earth (Replacement Dimension)", "url": "https://rickandmortyapi.com/api/location/20" }-->
 
       <!-- {{character.origin.name}} -->
       <!-- {{character.origin.url}} -->
 
-      <div>
-        <div class="episode" v-for="item in character.episode" :key="item.id">
-          <n-link :to="'/episodes/' + substr(item)">
-            <CardEpisode :episodeId="substr(item)" />
-          </n-link>
+      <div class="flex flex-wrap">
+        <div v-for="item in character.episode" :key="item.id">
+          <div class="episode text-white font-bold py-2 px-4 rounded">
+            <n-link :to="'/episodes/' + substr(item)">
+              <CardEpisode :episodeId="substr(item)" />
+            </n-link>
+          </div>
         </div>
       </div>
     </div>
@@ -58,9 +150,7 @@ export default {
   },
   created: async function () {
     await axios
-      .get(
-        "https://rickandmortyapi.com/api/character/" + this.idCharacter
-      )
+      .get("https://rickandmortyapi.com/api/character/" + this.idCharacter)
       .then((res) => {
         this.character = res.data;
       })
@@ -82,5 +172,16 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
+  background-color: #cbcbcb;
+  img {
+    width: 400px;
+  }
+}
+.episode {
+  background: linear-gradient(to bottom right, #9bca3d, #2e8841);
+  margin: 10px;
+}
+.episode:hover {
+  background: #8cc961;
 }
 </style>
