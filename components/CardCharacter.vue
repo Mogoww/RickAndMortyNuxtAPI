3ff66dfead728d9946a0f2c2ce8c21d8447e496c
Moggoww <gauthier.mauche@gmail.com>
2022-04-18 13:56:58 +0200

    Add responsive CharacterCard

--- components/CardCharacter.vue
+++ components/CardCharacter.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card">
-    <div>
+    <div class="image-div">
       <img
         class="image-character"
         :src="character.image"
@@ -43,7 +43,12 @@ export default {
   text-align: center;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
-  // width: 200px;
+  width: 400px;
+}
+.image-div {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .image-character {
   border-radius: 10px 10px 0 0;
@@ -57,5 +62,10 @@ export default {
 /* Add some padding inside the card container */
 .container {
   padding: 2px 16px;
+}
+@media only screen and (max-width: 600px) {
+  .card {
+    width: 150px;
+  }
 }
 </style>
