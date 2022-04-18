d27a7522e4b7df07f9b39c89d9bc8dfbdb93ad39
Moggoww <gauthier.mauche@gmail.com>
2022-04-18 21:40:08 +0200

    .

--- components/Character.vue
+++ components/Character.vue
@@ -78,15 +78,25 @@
                 </div>
 
                 <div class="flex-1 py-2 border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none" >
-                  <!-- {{ character }} -->
+                  {{ character.type }}
                 </div>
               </div>
-
-             
             </div>
           </div>
         </div>
       </div>
+
+      <div>
+        <h1>origin</h1>
+        <div v-if="character.origin">
+          {{ character.origin }}
+          {{ character.origin["name"] }}
+        </div>
+        <div v-if="character.origin">
+          {{ character.origin["url"] }}
+        </div>
+      </div>
+    
 
       <!-- <div>name: {{ character.name }}</div>
       <div>status: {{ character.status }}</div>
