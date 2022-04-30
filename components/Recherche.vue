5d6e8f8a581b3c5b508c142ac69113c0e4e0b58c
Moggoww <gauthier.mauche@gmail.com>
2022-04-30 12:16:23 +0200

    add filter

--- components/Recherche.vue
+++ components/Recherche.vue
@@ -6,7 +6,7 @@
           type="text"
           class="px-4 py-2 w-80"
           placeholder="Taper le nom ..."
-          id="recherche_text"
+          id="name"
         />
         <button
           v-on:click="recherche"
@@ -26,65 +26,74 @@
         </button>
       </div>
     </div>
+    <div v-if="typeRecherche == 'characters'" class="flex justify-center flex-wrap">
+      <div class="p-5">
+        <div>
+          <div class="relative inline-flex self-center">
+            <ion-icon
+              name="chevron-down"
+              class="absolute top-0 right-0 text-3xl rounded cursor-pointer"
+            ></ion-icon>
+            <select
+              id="status"
+              @change="status($event, 'status')"
+              class="font-bold rounded border-2 border-purple-700 text-gray-600 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none cursor-pointer"
+            >
+              <option value="" selected>Status :</option>
+              <option value="alive">🟢 En vie</option>
+              <option value="dead">🔴 Mort</option>
+              <option value="unknown">🔵 Non renseigné</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div class="p-5">
+        <div>
+          <div class="relative inline-flex self-center">
+            <ion-icon
+              name="chevron-down"
+              class="absolute top-0 right-0 text-3xl rounded cursor-pointer"
+            ></ion-icon>
+            <select
+              id="gender"
+              @change="status($event, 'gender')"
+              class="font-bold rounded border-2 border-purple-700 text-gray-600 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none cursor-pointer"
+            >
+              <option value="" selected>Genre :</option>
+              <option value="female">Femme</option>
+              <option value="male">Homme</option>
+              <option value="genderless">Sans sexe</option>
+              <option value="unknown">Non renseigné</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
 
-    <ul
-      class="grid grid-cols-3 gap-x-5 m-10 max-w-md mx-auto"
-      v-if="typeRecherche == 'characters'"
-    >
-      <li class="relative">
-        <input
-          class="sr-only peer"
-          type="radio"
-          value="alive"
-          name="answer"
-          id="answer_alive"
-          @click="status"
-        />
-        <label
-          class="flex p-5 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-green-500 peer-checked:ring-2 peer-checked:border-transparent"
-          for="answer_alive"
-          >En vie</label
-        >
-
-        <div class="absolute hidden w-5 h-5 peer-checked:block top-5 right-3">👍</div>
-      </li>
-
-      <li class="relative">
-        <input
-          class="sr-only peer"
-          type="radio"
-          value="dead"
-          name="answer"
-          id="answer_dead"
-          @click="status"
-        />
-        <label
-          class="flex p-5 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-red-500 peer-checked:ring-2 peer-checked:border-transparent"
-          for="answer_dead"
-          >Mort</label
-        >
-
-        <div class="absolute hidden w-5 h-5 peer-checked:block top-5 right-3">👎</div>
-      </li>
-
-      <li class="relative">
-        <input
-          class="sr-only peer"
-          type="radio"
-          value="unknown"
-          name="answer"
-          id="answer_unknown"
-          @click="status"
-        />
-        <label
-          class="flex p-5 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-red-500 peer-checked:ring-2 peer-checked:border-transparent"
-          for="answer_unknown"
-          >Non renseigné</label
-        >
-
-        <div class="absolute hidden w-5 h-5 peer-checked:block top-5 right-3">👎</div>
-      </li>
-    </ul>
+    <div v-if="typeRecherche == 'episodes'" class="flex justify-center flex-wrap">
+      <div class="p-5">
+        <div>
+          <div class="relative inline-flex self-center">
+            <ion-icon
+              name="chevron-down"
+              class="absolute top-0 right-0 text-3xl rounded cursor-pointer"
+            ></ion-icon>
+            <select
+              id="episode"
+              @change="status($event, 'episode')"
+              class="font-bold rounded border-2 border-purple-700 text-gray-600 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none cursor-pointer"
+            >
+              <option value="" selected>Saison :</option>
+              <option value="S01">Saison 01</option>
+              <option value="S02">Saison 02</option>
+              <option value="S03">Saison 03</option>
+              <option value="S04">Saison 04</option>
+              <option value="S05">Saison 05</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -100,12 +109,16 @@ export default {
   },
   methods: {
     recherche: function () {
-      this.name = document.querySelector("#recherche_text").value;
+      this.name = document.querySelector("#name").value;
       let data = {
         page: 1,
       };
       if (this.name && this.$route.query.name != this.name) data.name = this.name;
-      if (this.$route.query.status) data.status = this.$route.query.status;
+
+      let url = this.$route.query;
+      Object.keys(url).map(function (key) {
+        if (key != "page" && "name" != key) data[key] = url[key];
+      });
 
       this.$router.push({
         path: this.typeRecherche,
@@ -113,13 +126,17 @@ export default {
         query: data,
       });
     },
-    status: function (e) {
+    status: function (event, type) {
       let data = {
         page: 1,
       };
+      let url = this.$route.query;
+      Object.keys(url).map(function (key) {
+        if (key != "page" && type != key) data[key] = url[key];
+      });
+
       if (this.$route.query.name) data.name = this.$route.query.name;
-      if (!this.$route.query.status || this.$route.query.status != e.srcElement.value)
-        data.status = e.srcElement.value;
+      if (event.target.value != "") data[type] = event.target.value;
       this.$router.push({
         path: this.typeRecherche,
         replace: true,
@@ -128,12 +145,12 @@ export default {
     },
   },
   mounted: function () {
-    if (this.$route.query.name)
-      document.querySelector("#recherche_text").value = this.$route.query.name;
-    if(this.$route.query.status){
-      console.log(this.$route.query.status);
-      document.querySelector("#answer_"+this.$route.query.status).checked = true
-    }
+    let data = this.$route.query;
+    Object.keys(data).map(function (key) {
+      console.log(key);
+      let el = document.querySelector("#" + key);
+      if (el != null) el.value = data[key];
+    });
   },
 };
 </script>
